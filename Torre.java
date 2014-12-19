6ea82c892df4a87a1224be2c691020e589d3c5b4
Nacho Crespo <ignacio.crespo.martinez@gmail.com>
2014-12-19 19:15:21 +0100

    implementado metodos principales

--- Torre.java
+++ Torre.java
@@ -1,3 +1,28 @@
+import Avion;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.HashMap;
+
 public class Torre implements Runnable {
-    	public void run() { //...}
+
+	private BlockingQueue <Avion> q = new ArrayBlockingQueue<Avion> (1);
+	private HashMap <String,Avion> pistas = new HashMap <>();
+
+    	public void run() { 
+    		while(true) {
+    			Avion a = devolverPrimerAvion();
+    			a.asignarPista()
+    		}
+
+    	}
+
+    	public void meterAvionEnCola(Avion a) {
+    		q.put(a);
+    	}
+
+    	public Avion devolverPrimerAvion() {
+
+    		return q.take();
+    		 
+    	}
 }
